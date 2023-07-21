19a0790ae4ca2e8483c15d230f30ecae0393f82a
frdnhmrf <frdnhmrf@gmail.com>
2023-07-21 12:47:00 +0700

    model and migration

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
 }
